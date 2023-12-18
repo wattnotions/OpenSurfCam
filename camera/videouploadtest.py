fe6b443454d195151e6f4858ecb7224411b17961
wattnotions <shaneormonde@gmail.com>
2023-12-18 16:40:01 +0000

    added upload progress printout

--- camera/videouploadtest.py
+++ camera/videouploadtest.py
@@ -1,10 +1,11 @@
 import picamera
 import time
 import paramiko
+import os
 
 # Video recording settings
 output_filename = "output.h264"
-duration_seconds = 30  # Change this to your desired duration
+duration_seconds = 5  # Change this to your desired duration
 
 # SFTP settings
 remote_server = "18.201.217.84"  # Change to your remote server hostname/IP
@@ -13,21 +14,26 @@ remote_username = "bitnami"  # Change to your remote username
 remote_directory = "/home/bitnami/videos"  # Change to your desired remote directory
 pem_file_path = "/home/pi/OpenSurfCam/key.pem"  # Path to your .pem file
 
+# Callback function to print upload progress
+def print_progress(transferred, to_transfer):
+    print(f"Uploaded {transferred} out of {to_transfer} bytes ({transferred/to_transfer:.2%})")
+
 # Initialize the camera
 with picamera.PiCamera() as camera:
     camera.resolution = (640, 480)
     camera.framerate = 30
 
-    # Start recording
+    print("Video recording started.")
     camera.start_recording(output_filename)
 
     # Record for the specified duration
     camera.wait_recording(duration_seconds)
 
-    # Stop recording
     camera.stop_recording()
+    print("Video recording ended.")
 
 # Use the private key from the .pem file for authentication
+print("Initializing SSH connection...")
 private_key = paramiko.RSAKey.from_private_key_file(pem_file_path)
 
 # Establish a connection to the server
@@ -36,10 +42,12 @@ transport.connect(username=remote_username, pkey=private_key)
 
 # Open an SFTP session
 sftp = transport.open_sftp_client()
-sftp.put(output_filename, f"{remote_directory}/{output_filename}")
+
+print("Starting file upload...")
+sftp.put(output_filename, f"{remote_directory}/{output_filename}", callback=print_progress)
 
 # Close the SFTP connection
 sftp.close()
 transport.close()
 
-print(f"Video uploaded to {remote_directory}/{output_filename}")
+print(f"File upload complete. Video uploaded to {remote_directory}/{output_filename}")
